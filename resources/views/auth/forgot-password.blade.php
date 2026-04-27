dab53a1232077e8c0615122b7c25a37fc9834f87
Lilian <lilianyanguendji2002@gmail.com>
2026-04-27 19:00:49 +0100

    Modification de l'apparence des boutons de connexion et d'inscription

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="mb-4 text-sm text-gray-600" style="color:black;">
+        {{ __('Mot de passe oublié ? Pas de souci. Indiquez-nous simplement votre adresse e-mail et nous vous enverrons un lien de réinitialisation de mot de passe qui vous permettra d’en choisir un nouveau.') }}
     </div>
 
     <!-- Session Status -->
@@ -17,8 +17,8 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+            <x-primary-button style="background-color: #007bff; border-color: #007bff;">
+                {{ __('Réinitialiser') }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,7 @@
 
         <!-- Tel -->
         <div class="mt-4">
-            <x-input-label for="telephone" :value="__('Telephone')" />
+            <x-input-label for="telephone" :value="__('Téléphone')" />
             <x-text-input id="telephone" class="block mt-1 w-full" type="text" name="telephone" :value="old('telephone')" required autofocus autocomplete="telephone" />
             <x-input-error :messages="$errors->get('telephone')" class="mt-2" />
         </div>
@@ -32,7 +32,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Mot de passe')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -44,7 +44,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmer le mot de passe')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -55,11 +55,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Déja incrit') }}
             </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
+            <x-primary-button class="ms-4" style="background-color: #007bff; border-color: #007bff;">
+                {{ __('S\'inscrire') }}
             </x-primary-button>
         </div>
     </form>
